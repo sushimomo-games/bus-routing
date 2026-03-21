dbf4e5719153450c44bba8037f9a96701bca764e
emilyrhee <emilyrhee99@gmail.com>
2026-03-21 19:41:12 +0000

    refactor: made the route creation like not a part fo the RoadNodeArea

--- road/road-node/RoadNodeArea.cs
+++ road/road-node/RoadNodeArea.cs
@@ -9,12 +9,7 @@ using static Path;
 
 public partial class RoadNodeArea : Area2D
 {
-    /// <summary>
-    /// This exists so that we do not need to add a Route to LevelState until
-    /// it's fully created and valid.
-    /// </summary>
-    private static Route _tempRoute;
-
+    private Route _tempRoute; // Used during route creation before the route is finalized.
     /// <summary>
     /// Backup of the route's path for reverting invalid edits.
     /// </summary>
@@ -57,37 +52,38 @@ public partial class RoadNodeArea : Area2D
         if (@event.IsLeftMouseRelease()
         && (CurrentRouteCreationStep == AddingSubsequentStops || CurrentRouteCreationStep == EditingRoute))
         {
-            FinalizeRoute();
+            RouteEditor.FinalizeRouteCreation();
         }
     }
 
     private void _on_input_event(Node viewport, InputEvent @event, long shapeIdx)
     {
-        var clickedRoadNode = GetParent<RoadNode>();
+        var selectedRoadNode = GetParent<RoadNode>();
 
         if (@event.IsLeftMouseClick())
         {
-            if (SelectedRoute != null)
-            {
-                if (SelectedRoute.Path.First() == clickedRoadNode
-                 || SelectedRoute.Path.Last() == clickedRoadNode)
-                {
-                    IsEditingFromStart = SelectedRoute.Path.First() == clickedRoadNode;
-                    StartRouteEdit(SelectedRoute, clickedRoadNode);
-                    return;
-                }
-            }
+        //     if (SelectedRoute != null)
+        //     {
+        //         if (SelectedRoute.Path.First() == clickedRoadNode
+        //          || SelectedRoute.Path.Last() == clickedRoadNode)
+        //         {
+        //             IsEditingFromStart = SelectedRoute.Path.First() == clickedRoadNode;
+        //             StartRouteEdit(SelectedRoute, clickedRoadNode);
+        //             return;
+        //         }
+        //     }
 
-            if (clickedRoadNode is BusStop && CurrentRouteCreationStep == NotCreating)
+            if (selectedRoadNode is BusStop && CurrentRouteCreationStep == NotCreating)
             {
-                StartRouteCreation(clickedRoadNode);
+                // StartRouteCreation(selectedRoadNode);
+                RouteEditor.StartRouteCreation(selectedRoadNode);
             }
         }
         else if (@event is InputEventMouseMotion)
         {
             if (CurrentRouteCreationStep == AddingSubsequentStops || CurrentRouteCreationStep == EditingRoute)
             {
-                ContinueRoute(clickedRoadNode);
+                RouteEditor.ContinueRoute(selectedRoadNode);
             }
         }
     }
@@ -114,94 +110,6 @@ public partial class RoadNodeArea : Area2D
         CurrentLevel.AddChild(RoutePreviewLine);
     }
 
-    private void StartRouteCreation(RoadNode startNode)
-    {
-        GD.Print("Route creation started at: " + startNode.Name);
-        CurrentRouteCreationStep = AddingSubsequentStops;
-
-        _tempRoute = new Route();
-        _tempRoute.AppendNode(startNode);
-
-        _tempRoute.Visual = new RouteVisual(_tempRoute);
-        CurrentLevel.AddChild(_tempRoute.Visual);
-
-        RoutePreviewLine = CreateLineAt(startNode.GlobalPosition);
-        RoutePreviewLine.DefaultColor = _tempRoute.Color;
-        CurrentLevel.AddChild(RoutePreviewLine);
-    }
-
-    private void ContinueRoute(RoadNode nextNode)
-    {
-        Route routeToEdit = (CurrentRouteCreationStep == EditingRoute) ? SelectedRoute : _tempRoute;
-        
-        if (routeToEdit == null) return;
-
-        RoadNode lastNode;
-        if (IsEditingFromStart)
-        {
-            lastNode = routeToEdit.Path.FirstOrDefault();
-        }
-        else
-        {
-            lastNode = routeToEdit.Path.LastOrDefault();
-        }
-
-        if (lastNode == nextNode) // Prevent adding the same node twice
-        {
-            return;
-        }
-
-        if (IsEditingFromStart)
-        {
-            routeToEdit.PrependNode(nextNode);
-            RoutePreviewLine.SetPointPosition(0, nextNode.GlobalPosition);
-            RoutePreviewLine.AddPoint(nextNode.GlobalPosition, 0);
-        }
-        else
-        {
-            routeToEdit.AppendNode(nextNode);
-            RoutePreviewLine.SetPointPosition(RoutePreviewLine.GetPointCount() - 1, nextNode.GlobalPosition);
-            RoutePreviewLine.AddPoint(nextNode.GlobalPosition);
-        }
-    }
-
-    private void FinalizeRoute()
-    {
-        if (CurrentRouteCreationStep == EditingRoute)
-        {
-            FinalizeRouteEdit();
-        }
-        else if (CurrentRouteCreationStep == AddingSubsequentStops)
-        {
-            FinalizeRouteCreation();
-        }
-    }
-
-    private void FinalizeRouteCreation()
-    {
-        var lastNode = _tempRoute.Path[^1];
-        if (_tempRoute.Path.Count < 2 || lastNode is not BusStop)
-        {
-            errorMessage.DisplayMessage("Route must start and end at a bus stop.");
-            ReturnRouteColor(new KeyValuePair<string, Color>(_tempRoute.ColorName, _tempRoute.Color));
-            _tempRoute.Visual?.QueueFree();
-        }
-        else
-        {
-            LevelState.AllRoutes.Add(_tempRoute);
-            var routeList = GetTree().CurrentScene.GetNode<ItemList>(Path.RouteListNode);
-            routeList.AddItem(_tempRoute.ColorName + " line");
-            LevelState.UpdateAllHouseStatuses();
-            LevelState.RefreshAllRouteVisuals();
-        }
-        foreach (var route in LevelState.AllRoutes)
-        {
-            GD.Print($"Route {route.ColorName}: {string.Join(" -> ", route.Path.Select(n => n.Name))}");
-        }
-        _tempRoute = null;
-        ResetState();
-    }
-
     private void FinalizeRouteEdit()
     {
         var editedRoute = SelectedRoute;
@@ -220,14 +128,6 @@ public partial class RoadNodeArea : Area2D
         }
         LevelState.RefreshAllRouteVisuals();
         _routeBackup = null;
-        ResetState();
-    }
-
-    private void ResetState()
-    {
-        RoutePreviewLine?.QueueFree();
-        RoutePreviewLine = null;
-        CurrentRouteCreationStep = NotCreating;
-        IsEditingFromStart = false;
+        // ResetState();
     }
 }
--- route/RouteEditor.cs
+++ route/RouteEditor.cs
@@ -1,7 +1,76 @@
 using Godot;
 using System.Collections.Generic;
+using System.Linq;
+using static EditorState;
+using static LevelState;
+using static LineFactory;
+using static Path;
+using static RouteCreationStep;
 
 public partial class RouteEditor : Node
 {
-    public static List<RoadNode> SelectedRoadNodes { get; set; } = [];
+    // public static List<RoadNode> SelectedRoadNodes { get; set; } = [];
+
+    /// <summary>
+    /// This exists so that we do not need to add a Route to LevelState until
+    /// it's fully created and valid.
+    /// </summary>
+    private static Route _routeInProgress;
+
+    public static void StartRouteCreation(RoadNode startNode)
+    {
+        GD.Print("Route creation started at: " + startNode.Name);
+        CurrentRouteCreationStep = RouteCreationStep.AddingSubsequentStops;
+        _routeInProgress = new Route();
+        _routeInProgress.AppendNode(startNode);
+
+        _routeInProgress.Visual = new RouteVisual(_routeInProgress);
+        CurrentLevel.AddChild(_routeInProgress.Visual);
+
+        RoutePreviewLine = CreateLineAt(startNode.GlobalPosition);
+        RoutePreviewLine.DefaultColor = _routeInProgress.Color;
+        CurrentLevel.AddChild(RoutePreviewLine);
+    }
+
+    public static void ContinueRoute(RoadNode nextNode)
+    {
+        GD.Print("Continuing route to: " + nextNode.Name);
+        _routeInProgress.AppendNode(nextNode);
+        RoutePreviewLine.SetPointPosition(RoutePreviewLine.GetPointCount() - 1, nextNode.GlobalPosition);
+        RoutePreviewLine.AddPoint(nextNode.GlobalPosition);
+    }
+
+    public static void FinalizeRouteCreation()
+    {
+        RoadNode lastNode = _routeInProgress.Path[^1];
+
+        if (_routeInProgress.Path.Count < 2 || lastNode is not BusStop)
+        {
+            CurrentLevel.GetNode<ErrorMessage>(Path.ErrorMessageNode).DisplayMessage("Route must start and end at a bus stop");
+            ReturnRouteColor(new KeyValuePair<string, Color>(_routeInProgress.ColorName, _routeInProgress.Color));
+            _routeInProgress.Visual?.QueueFree();
+        }
+        else
+        {
+            LevelState.AllRoutes.Add(_routeInProgress);
+            var routeList = CurrentLevel.GetNode<ItemList>(Path.RouteListNode);
+            routeList.AddItem(_routeInProgress.ColorName + " line");
+            LevelState.UpdateAllHouseStatuses();
+            LevelState.RefreshAllRouteVisuals();
+        }
+        foreach (var route in LevelState.AllRoutes)
+        {
+            GD.Print($"Route {route.ColorName}: {string.Join(" -> ", route.Path.Select(n => n.Name))}");
+        }
+        ResetState();
+    }
+
+    private static void ResetState()
+    {
+        _routeInProgress = null;
+        RoutePreviewLine?.QueueFree();
+        RoutePreviewLine = null;
+        CurrentRouteCreationStep = NotCreating;
+        IsEditingFromStart = false;
+    }
 }
