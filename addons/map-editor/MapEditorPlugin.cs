f35b226a57c205288b665b862aa2609f9803d1be
Mitch Palczewski <mitchellpalczewski@gmail.com>
2026-04-20 12:15:19 -0400

    Editor: Replaced RoadNetworkPlugin with more generic MapEditorPlugin

--- addons/map-editor/MapEditorPlugin.cs
+++ addons/map-editor/MapEditorPlugin.cs
@@ -1,29 +1,36 @@
 #if TOOLS
 using Godot;
+using System.Collections.Generic;
 using System.Linq;
 
 [Tool]
-public partial class RoadNetworkPlugin : EditorPlugin
+public partial class MapEditorPlugin : EditorPlugin
 {
-    private PackedScene _intersectionScene;
+    private Dictionary<ToolMode, PackedScene> _scenes = new();
     private OptionButton _modeSelector;
     private Button _toggleBtn;
-    private bool _roadModeActive = false;
+    private bool _toolActive = false;
     private RoadNode _connectionSource = null;
-    private enum ToolMode { Place, Connect }
+    private enum ToolMode { PlaceRoad, ConnectRoad, PlaceHouse, PlaceDestination }
 
-    public override bool _Handles(GodotObject @object) => _roadModeActive;
+    public override bool _Handles(GodotObject @object) => _toolActive;
     public override void _EnterTree()
     {
-        _intersectionScene = GD.Load<PackedScene>("res://road/intersection/intersection-node.tscn");
-        var container = new HBoxContainer();
+        _scenes[ToolMode.PlaceRoad] = GD.Load<PackedScene>("res://road/intersection/intersection-node.tscn");
+        _scenes[ToolMode.PlaceHouse] = GD.Load<PackedScene>("res://buildings/house/house.tscn");
+        _scenes[ToolMode.PlaceDestination] = GD.Load<PackedScene>("res://buildings/destination/destination.tscn");
 
+        var container = new HBoxContainer();
         _toggleBtn = new Button { Text = "Road Tool", ToggleMode = true };
         _toggleBtn.Toggled += OnToolToggled;
 
         _modeSelector = new OptionButton();
-        _modeSelector.AddItem("Mode: Place Nodes", (int)ToolMode.Place);
-        _modeSelector.AddItem("Mode: Connect Nodes", (int)ToolMode.Connect);
+        _modeSelector.AddItem("Road: Place Nodes", (int)ToolMode.PlaceRoad);
+        _modeSelector.AddItem("Road: Connect Nodes", (int)ToolMode.ConnectRoad);
+        _modeSelector.AddItem("Build: House DNU", (int)ToolMode.PlaceHouse);
+        _modeSelector.AddItem("Build: Buisness DNU", (int)ToolMode.PlaceDestination);
+
+        _modeSelector.Visible = false;
 
         container.AddChild(_toggleBtn);
         container.AddChild(_modeSelector);
@@ -32,45 +39,57 @@ public partial class RoadNetworkPlugin : EditorPlugin
     }
     public override void _ExitTree()
     {
-        if (_toggleBtn != null)
+        if (IsInstanceValid(_toggleBtn))
         {
-            _toggleBtn.Toggled -= OnToolToggled;
+            if (_toggleBtn.IsConnected(Button.SignalName.Toggled, Callable.From<bool>(OnToolToggled)))
+            {
+                _toggleBtn.Toggled -= OnToolToggled;
+            }
         }
-        _modeSelector?.GetParent()?.QueueFree();
+
+        var parent = _modeSelector?.GetParent();
+        if (IsInstanceValid(parent))
+        {
+            parent.QueueFree();
+        }
     }
     private void OnToolToggled(bool toggled)
     {
-        _roadModeActive = toggled;
+        _toolActive = toggled;
         _connectionSource = null;
-        GD.Print(_roadModeActive ? "Road Mode: ON" : "Road Mode: OFF");
+        if (_modeSelector != null)
+        {
+            _modeSelector.Visible = toggled;
+        }
+        GD.Print(_toolActive ? "Road Mode: ON" : "Road Mode: OFF");
     }
+
     public override bool _ForwardCanvasGuiInput(InputEvent @event)
     {
-        if (!_roadModeActive) return false;
-
+        if (!_toolActive) return false;
         if (@event is InputEventMouseButton mb && mb.Pressed && mb.ButtonIndex == MouseButton.Left && mb.CtrlPressed)
         {
-
             var sceneRoot = EditorInterface.Singleton.GetEditedSceneRoot();
             if (sceneRoot == null) return false;
-
             Vector2 worldPos = EditorInterface.Singleton.GetEditorViewport2D().GetFinalTransform().AffineInverse() * mb.Position;
-            var clickedNode = FindNodeAtPosition(sceneRoot, worldPos);
-            if (_modeSelector.Selected == (int)ToolMode.Place)
-            {
-                if (clickedNode == null) CreateNode(sceneRoot, worldPos);
-            }
-            else
-            {
-                if (clickedNode != null) HandleConnection(clickedNode);
-            }
 
+            ToolMode currentMode = (ToolMode)_modeSelector.Selected;
+
+            switch (currentMode)
+            {
+                case ToolMode.PlaceRoad:
+                    CreateRoadNode(sceneRoot, worldPos);
+                    break;
+                case ToolMode.ConnectRoad:
+                    HandleConnection(sceneRoot, worldPos);
+                    break;
+            }
             return true;
         }
         return false;
     }
 
-    private void CreateNode(Node sceneRoot, Vector2 worldPos)
+    private void CreateRoadNode(Node sceneRoot, Vector2 worldPos)
     {
         Node container = sceneRoot.FindChild("IntersectionNodes", true, false);
 
@@ -82,13 +101,13 @@ public partial class RoadNetworkPlugin : EditorPlugin
             GD.Print("Created 'IntersectionNodes' container.");
         }
 
-        if (_intersectionScene == null)
+        if (_scenes[ToolMode.PlaceRoad] == null)
         {
             GD.PrintErr("CRITICAL: Intersection.tscn could not be loaded at res://Intersection.tscn");
             return;
         }
 
-        var newNode = _intersectionScene.Instantiate<IntersectionNode>();
+        var newNode = _scenes[ToolMode.PlaceRoad].Instantiate<IntersectionNode>();
 
         container.AddChild(newNode);
         newNode.Owner = sceneRoot;
@@ -97,8 +116,10 @@ public partial class RoadNetworkPlugin : EditorPlugin
         GD.Print($"Placed new Node under {container.Name}.");
     }
 
-    private void HandleConnection(RoadNode clickedNode)
+    private void HandleConnection(Node sceneRoot, Vector2 worldPos)
     {
+        var clickedNode = FindNodeAtPosition(sceneRoot, worldPos);
+        if (clickedNode == null) return;
         if (_connectionSource == null)
         {
             _connectionSource = clickedNode;
@@ -130,6 +151,10 @@ public partial class RoadNetworkPlugin : EditorPlugin
             _connectionSource = null;
         }
     }
+
+
+
+
 
     private RoadNode FindNodeAtPosition(Node root, Vector2 pos)
     {
